e1fecd0927f2af98f930c8c4d05d78e52f365955
Phil Alegret <philip.alegret@gmail.com>
2020-12-13 10:08:39 -0500

    Content for lesson 3.

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
   data() {
     return {
       product: 'Socks',
-      description: 'A comfy pair of socks.'
+      description: 'A comfy pair of socks.',
+      image: './assets/images/socks_green.jpg',
+      url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3/'
     }
   }
 })
