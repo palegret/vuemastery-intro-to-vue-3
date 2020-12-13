0633c6600b1a17071e43986664fca95be28fa7ac
Phil Alegret <philip.alegret@gmail.com>
2020-12-13 11:37:26 -0500

    Content for lesson 6.

--- main.js
+++ main.js
@@ -9,10 +9,23 @@ const app = Vue.createApp({
       onSale: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 2234, color: 'green' },
-        { id: 2235, color: 'blue' }
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
       ],
-      sizes: ['S', 'M', 'L', 'XL']
+      sizes: ['S', 'M', 'L', 'XL'],
+      cart: 0
+    }
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    removeFromCart() {
+      this.cart -= 1
+      this.cart = this.cart < 0 ? 0 : this.cart
+    },
+    updateImage(variantImage) {
+      this.image = variantImage
     }
   }
 })
