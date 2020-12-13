a6fc1256cfae1be723585e3b745cac5344d7640b
Phil Alegret <philip.alegret@gmail.com>
2020-12-13 11:11:18 -0500

    Content for lesson 5.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const app = Vue.createApp({ 
+const app = Vue.createApp({
   data() {
     return {
       product: 'Socks',
@@ -6,7 +6,13 @@ const app = Vue.createApp({
       image: './assets/images/socks_green.jpg',
       inStock: true,
       inventory: 100,
-      onSale: true
+      onSale: true,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'green' },
+        { id: 2235, color: 'blue' }
+      ],
+      sizes: ['S', 'M', 'L', 'XL']
     }
   }
 })
